8046d1f7c914248062ccbe387bc2479d4334a245
Emile Sonneveld <emile.sonneveld@vito.be>
2025-06-02 16:08:42 +0200

    insar_preprocessing_v02

--- openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/custom_processes.py
+++ openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/custom_processes.py
@@ -322,6 +322,65 @@ def insar_preprocessing(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     )
 
 
+@non_standard_process(
+    ProcessSpec(
+        id="insar_preprocessing_v02",
+        description="Proof-of-concept process to run CWL based inSAR. More info here: https://github.com/cloudinsar/s1-workflows",
+    )
+    .param(name="burst_id", description="burst_id", schema={"type": "integer"}, required=True)
+    .param(name="sub_swath", description="sub_swath", schema={"type": "string"}, required=True)
+    .param(
+        name="temporal_extent",
+        description="temporal_extent",
+        schema={
+            "type": "array",
+            "subtype": "temporal-interval",
+            "uniqueItems": True,
+            "minItems": 2,
+            "maxItems": 2,
+            "items": {
+                "anyOf": [
+                    {
+                        "type": "string",
+                        "format": "date-time",
+                        "subtype": "date-time",
+                        "description": "Date and time with a time zone.",
+                    },
+                    {
+                        "type": "string",
+                        "format": "date",
+                        "subtype": "date",
+                        "description": "Date only, formatted as `YYYY-MM-DD`. The time zone is UTC. Missing time components are all 0.",
+                    },
+                    {
+                        "type": "string",
+                        "subtype": "time",
+                        "pattern": "^\\d{2}:\\d{2}:\\d{2}$",
+                        "description": "Time only, formatted as `HH:MM:SS`. The time zone is UTC.",
+                    },
+                    {"type": "null"},
+                ]
+            },
+        },
+        required=True,
+    )
+    .param(
+        name="master_date",
+        description="master_date",
+        schema={"type": "string", "subtype": "date"},
+        required=True,
+    )
+    .param(name="polarization", description="polarization", schema={"type": "string"}, required=False)
+    .returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
+)
+def insar_preprocessing_v02(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
+    return insar_common(
+        args,
+        env,
+        "https://raw.githubusercontent.com/cloudinsar/s1-workflows/refs/heads/main/cwl/insar_preprocessing_v02.cwl",
+    )
+
+
 SAR_BACKSCATTER_COEFFICIENT_DEFAULT = "sigma0-ellipsoid"
 
 
