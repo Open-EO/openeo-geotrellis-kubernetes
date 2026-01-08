1c1dfd215c4c73be7419fa6e1fde374d714e700a
Emile Sonneveld <emile.sonneveld@vito.be>
2026-01-08 17:20:34 +0100

    More params

--- openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/custom_processes.py
+++ openeo-geopyspark-k8s-custom-processes/src/openeo_geopyspark_k8s_custom_processes/custom_processes.py
@@ -328,7 +328,25 @@ def sar_coherence(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     ProcessSpec(
         id="sar_coherence_parallel",
         description="Proof-of-concept process to run CWL based inSAR. More info here: https://github.com/cloudinsar/s1-workflows",
-    ).returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
+    )
+    .param(
+        name="InSAR_pairs",
+        description="InSAR_pairs",
+        schema={
+            "type": "array",
+            "items": {
+                "type": "array",
+                "items": {"type": "string"},
+            },
+        },
+        required=True,
+    )
+    .param(name="burst_id", description="burst_id", schema={"type": "integer"}, required=True)
+    .param(name="coherence_window_az", description="coherence_window_az", schema={"type": "integer"}, required=False)
+    .param(name="coherence_window_rg", description="coherence_window_rg", schema={"type": "integer"}, required=False)
+    .param(name="polarization", description="polarization", schema={"type": "string"}, required=True)
+    .param(name="sub_swath", description="sub_swath", schema={"type": "string"}, required=True)
+    .returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
 )
 def sar_coherence_parallel(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     return cwl_common(
@@ -345,7 +363,27 @@ def sar_coherence_parallel(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     ProcessSpec(
         id="sar_interferogram",
         description="Proof-of-concept process to run CWL based inSAR. More info here: https://github.com/cloudinsar/s1-workflows",
-    ).returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
+    )
+    .param(
+        name="InSAR_pairs",
+        description="InSAR_pairs",
+        schema={
+            "type": "array",
+            "items": {
+                "type": "array",
+                "items": {"type": "string"},
+            },
+        },
+        required=True,
+    )
+    .param(name="burst_id", description="burst_id", schema={"type": "integer"}, required=True)
+    .param(name="coherence_window_az", description="coherence_window_az", schema={"type": "integer"}, required=False)
+    .param(name="coherence_window_rg", description="coherence_window_rg", schema={"type": "integer"}, required=False)
+    .param(name="n_az_looks", description="n_az_looks", schema={"type": "integer"}, required=False)
+    .param(name="n_rg_looks", description="n_rg_looks", schema={"type": "integer"}, required=False)
+    .param(name="polarization", description="polarization", schema={"type": "string"}, required=True)
+    .param(name="sub_swath", description="sub_swath", schema={"type": "string"}, required=True)
+    .returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
 )
 def sar_interferogram(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     return cwl_common(
@@ -362,7 +400,23 @@ def sar_interferogram(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     ProcessSpec(
         id="sar_slc_preprocessing",
         description="Proof-of-concept process to run CWL based inSAR. More info here: https://github.com/cloudinsar/s1-workflows",
-    ).returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
+    )
+    .param(
+        name="temporal_extent",
+        description="temporal_extent",
+        schema={"type": "array", "items": {"type": "string"}},
+        required=True,
+    )
+    .param(name="burst_id", description="burst_id", schema={"type": "integer"}, required=True)
+    .param(name="primary_date", description="primary_date", schema={"type": "string"}, required=True)
+    .param(
+        name="polarization",
+        description="polarization",
+        schema={"type": "array", "items": {"type": "string"}},
+        required=True,
+    )
+    .param(name="sub_swath", description="sub_swath", schema={"type": "string"}, required=False)
+    .returns(description="the data as a data cube", schema={"type": "object", "subtype": "datacube"})
 )
 def sar_slc_preprocessing(args: ProcessArgs, env: EvalEnv) -> DriverDataCube:
     return cwl_common(
